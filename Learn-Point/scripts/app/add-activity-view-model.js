41ffd8ebd20fad674b79854504f16e3cf5e3a53a
Daniel Filipov <Drgreen_bg@yahoo.com>
2013-10-25 19:07:29 -0500

    Map in add activity

--- Learn-Point/scripts/app/add-activity-view-model.js
+++ Learn-Point/scripts/app/add-activity-view-model.js
@@ -8,6 +8,7 @@ var addActivityViewModel = (function () {
     var show = function () {
         $newStatus.val('');
         validator.hideMessages();
+        showMap();
     };
     var saveActivity = function () {
         if (validator.validate()) {
@@ -21,6 +22,40 @@ var addActivityViewModel = (function () {
             activities.sync();
         }
     };
+    
+    var showMap = function() {
+        var lon;
+        var lat;
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        
+        function onSuccess(position) {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            generateMap(lat, lon);
+        };
+        
+        function onError(error) {
+            lon = -87.6500523;
+            lat = 43.465187;
+            generateMap(lat, lon);
+        };
+        
+        function generateMap(lat, lon) {
+            directionsDisplay = new google.maps.DirectionsRenderer();
+            var position = new google.maps.LatLng(lat, lon);
+            var mapOptions = {
+                zoom:7,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                center: position
+            }
+            console.log(position);
+            console.log(lat);
+            console.log(lon);
+            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            directionsDisplay.setMap(map);
+        }
+    };
+    
     return {
         init: init,
         show: show,
